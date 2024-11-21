8ff681ffc95039fdefe1a5a8d1136aedff6806eb
prakash-2002 <prakashslm27@gmail.com>
2024-11-21 13:07:36 +0530

    multer upload (middleware) is created

---
+++ backend/utils/middlewares/upload.js
@@ -0,0 +1,28 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '../../e-commerce-dashboard/public/products');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 2 * 1024 * 1024 },
+    fileFilter: function (req, file, cb) {
+        const fileTypes = /jpg|jpeg|png/;
+        const isMimeTypeValid = fileTypes.test(file.mimetype);
+        const isExtNameValid = fileTypes.test(path.extname(file.originalname).toLowerCase());
+
+        if (isMimeTypeValid && isExtNameValid) {
+            return cb(null, true);
+        }
+        cb(new Error("Only images (jpg, jpeg, png) are allowed"));
+    }
+});
+
+module.exports = upload;
