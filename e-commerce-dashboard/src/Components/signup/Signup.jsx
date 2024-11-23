a47830400880f91bcfba46d39b258f51c8edc6b7
prakash-2002 <prakashslm27@gmail.com>
2024-11-23 14:52:26 +0530

    error state is added  and tost msg is added

--- e-commerce-dashboard/src/Components/signup/Signup.jsx
+++ e-commerce-dashboard/src/Components/signup/Signup.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "@mui/material";
+import { Button, CircularProgress } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { FaRegUser } from "react-icons/fa";
 import { MdOutlineMail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
 import logo from '../../assets/logo/logo-removebg-preview.png'
+import { toast } from "react-toastify";
+
 const Signup = () => {
   const navigate = useNavigate();
+  const [error, setError]=useState(null);
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     username: "",
     email: "",
@@ -16,6 +20,7 @@ const Signup = () => {
   });
 
   const handleChange = (e) => {
+    setError(null)
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
@@ -24,24 +29,27 @@ const Signup = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLoading(true);
     console.log("User Data:", formData);
-    console.log(import.meta.env.VITE_API_URL + "/register",'routes');
+    // console.log(import.meta.env.VITE_API_URL + "/register",'routes');
     axios
       .post(import.meta.env.VITE_API_URL + "/register", formData)
       .then((res) => {
         if (res.data.success) {
           console.log("Registration successful");
           localStorage.setItem("isRegistered", "true"); // Set a flag in localStorage
+          toast.success("Registration successful", { theme: "colored" });
           navigate("/login");
           // Handle successful registration, and redirect the user
         }
       })
-      .catch((err) => {
-        if (err.response && err.response.status === 400) {
-          alert(err.response.data.message); // Show the duplicate email error
-        } else {
-          console.log(err);
-        }
+      .catch((error) => {
+        console.error("Error signup:", error.response?.data?.message || error.message);
+        setError(error.response?.data?.message || error.message)
+
+      })
+      .finally(() => {
+        setLoading(false);
       });
   };
 
@@ -53,6 +61,7 @@ const Signup = () => {
   {/* Signup Form Section */}
   <div className="signupPage d-flex flex-column p-4 shadow-lg">
     <h2 className="text-center">Signup</h2>
+   {error&& <p style={{color:"red"}}>{error}</p>}
 
     {/* Signup Form */}
     <form onSubmit={handleSubmit} className="mt-3">
@@ -97,7 +106,7 @@ const Signup = () => {
           type="password"
           className="form-control"
           name="password"
-          placeholder="Password"
+          placeholder="Password must be 7 char"
           value={formData.password}
           onChange={handleChange}
           required
@@ -105,9 +114,17 @@ const Signup = () => {
       </div>
 
       {/* Signup Button */}
-      <Button variant="contained" type="submit" className="btnSignup btn-primary mt-3 w-100">
-        Signup
-      </Button>
+      {/* <Button variant="contained" type="submit" className="btnSignup btn-primary mt-3 w-100">
+       {loading?"Loading...": Signup}
+      </Button> */}
+      <Button
+            type="submit"
+            className="btnSignup btn-primary mt-3 w-100"
+            disabled={loading} fullWidth
+          >
+            {loading ? <CircularProgress size={24} /> : "Signup"}{" "}
+            {/* Show loader when submitting */}
+          </Button>
     </form>
 
     {/* Already Have an Account */}
