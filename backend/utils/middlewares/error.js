b8dcf40dbea1ec3f2d3508e5ed2d5c738635f937
prakash-2002 <prakashslm27@gmail.com>
2024-12-07 11:56:16 +0530

    duplicate img del

--- backend/utils/middlewares/error.js
+++
@@ -1,7 +0,0 @@
-module.exports=(err,req,res,next)=>{
-    err.statusCode=err.statusCode ||500;
-    res.statusCode(err.statusCode).json({
-       success:false,
-       message:err.message 
-    })
-} 
