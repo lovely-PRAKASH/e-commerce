a28cca6e68f851555c60595af186a787ad9d143f
prakash-2002 <prakashslm27@gmail.com>
2024-12-07 11:51:45 +0530

    cors origin has updated and static url is changed

--- backend/app.js
+++ backend/app.js
@@ -17,12 +17,12 @@ const payment=require("./routes/payment")
 
 app.use(express.json());
 app.use(cors({
-  origin:"*",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials:true,
 }));
 connectDatabase();
 // app.use(express.static("../../assets/Profile"))
-app.use('/addproduct', express.static(path.join(__dirname,'uploads') ) )
+app.use('/uploads', express.static(path.join(__dirname,'uploads') ) )
 
 app.use("/api/v1/", products);
 app.use("/api/v1/", orders);
